9173894f3113dee20120cb1d7d637a4ed8f85985
MarlonGrandy <106160715+MarlonGrandy@users.noreply.github.com>
2023-10-12 08:41:48 -0400

    Update data_cleaning.py

--- data_processing/data_cleaning.py
+++ data_processing/data_cleaning.py
@@ -2,10 +2,9 @@ import pandas as pd
 import os
 import geopandas
 
-
 cwd = os.getcwd()
 
-
+# Function to clean DEM data
 def clean_DEM_data():
     DEM_data = pd.read_csv(os.path.join(
         cwd, "dashboard_data", "RIDEMCochlodiniumCounts.csv"))
@@ -14,70 +13,61 @@ def clean_DEM_data():
     DEM_data['Date'] = pd.to_datetime(DEM_data['Date'])
     DEM_data['Cochlodinium polykrikoides (cells/L)'] = DEM_data['Cochlodinium polykrikoides (cells/L)'].str.replace(
         ',', '').astype(float)
-
     DEM_data.rename(columns={
                     'Cochlodinium polykrikoides (cells/L)': 'Margalefidinium polykrikoides (Cells/L)'}, inplace=True)
     DEM_data = DEM_data.set_index('Date')
     return DEM_data
 
-
+# Function to clean URI data
 def clean_URI_data():
     xls = pd.ExcelFile(os.path.join(cwd, "dashboard_data",
                                     'Phytoplankton Count Data (.xls)'))
     URI_data = pd.read_excel(xls, 'Count data')
     URI_data = URI_data.drop(index=0)
-
     URI_data = URI_data[['DATE', 'Cochlodinium']]
     URI_data['DATE'] = pd.to_datetime(URI_data['DATE'])
     URI_data['Cochlodinium'] = URI_data['Cochlodinium'].astype(float)
     URI_data.rename(columns={
                     'Cochlodinium': 'Margalefidinium polykrikoides (Cells/L)', 'DATE': 'Date'}, inplace=True)
     URI_data = URI_data.set_index('Date')
-    URI_data = URI_data.resample(
-        'W').max()
+    URI_data = URI_data.resample('W').max()
     URI_data['Lat'] = 41.5722
     URI_data['Long'] = -71.3944
     return URI_data
 
-
+# Function to clean WHOI data
 def clean_WHOI_data():
     WHOI_data = pd.read_csv(os.path.join(
         cwd, "dashboard_data", "habhub_data.csv"))
     WHOI_data = WHOI_data[['DateTime', 'Margalefidinium polykrikoides']]
     WHOI_data['DateTime'] = pd.to_datetime(WHOI_data['DateTime'])
-    WHOI_data['Margalefidinium polykrikoides'] = WHOI_data['Margalefidinium polykrikoides'].astype(
-        float)
+    WHOI_data['Margalefidinium polykrikoides'] = WHOI_data['Margalefidinium polykrikoides'].astype(float)
     WHOI_data.rename(columns={
         'DateTime': 'Date', "Margalefidinium polykrikoides": 'Margalefidinium polykrikoides (Cells/L)'}, inplace=True)
-
     WHOI_data = WHOI_data.set_index('Date')
-    WHOI_data = WHOI_data.resample(
-        'W').max()
+    WHOI_data = WHOI_data.resample('W').max()
     WHOI_data['Lat'] = 41.49224
     WHOI_data['Long'] = -71.41896
     return WHOI_data
 
-
+# Function to clean meteorological data
 def clean_met_data():
     met_data = pd.read_csv(os.path.join(
         cwd, 'dashboard_data', 'NARPCMET.csv'), skiprows=[0, 1])
-
     met_data = met_data[['DateTimeStamp', 'TotPrcp', 'MaxWSpd', 'WSpd']]
     met_data['DateTimeStamp'] = pd.to_datetime(met_data['DateTimeStamp'])
     met_data = met_data.set_index("DateTimeStamp")
-    MaxWSpd = met_data['MaxWSpd'].resample(
-        'W').max()
+    MaxWSpd = met_data['MaxWSpd'].resample('W').max()
     WSpd = met_data['WSpd'].resample('W').mean()
     TotPrcp = met_data['TotPrcp'].resample('D').max().resample('W').sum()
     df = pd.concat([MaxWSpd, WSpd, TotPrcp], axis=1)
     df['Date'] = df.index.values
-    df['Date'] = df['Date'] - pd.to_timedelta(
-        df['Date'].dt.dayofweek, unit='d')
+    df['Date'] = df['Date'] - pd.to_timedelta(df['Date'].dt.dayofweek, unit='d')
     df.reset_index(inplace=True, drop=True)
     df = df.rename({'DateTimeStamp': 'Date'})
     return df
 
-
+# Function to clean water quality data
 def clean_WQ_data():
     WQ_data = pd.read_csv(
         '/Users/marlongrandy/Desktop/rust-tide-dashboard/dashboard_data/NARPCWQ.csv', skiprows=[0, 1])
@@ -90,11 +80,10 @@ def clean_WQ_data():
         WQ_data['Date'].dt.dayofweek, unit='d')
     WQ_data.reset_index(inplace=True, drop=True)
     WQ_data['Temp'] = WQ_data['Temp'].apply(lambda x: x*1.8+32)
-
     WQ_data = WQ_data.rename({'Temp': 'WaterTemp', 'DateTimeStamp': 'Date'})
     return WQ_data
 
-
+# Function to create a consolidated dataframe for the dashboard
 def dashbaord_df():
     df = pd.concat([clean_DEM_data(), clean_URI_data(),
                    clean_WHOI_data()]).sort_values(by='Date')
@@ -107,14 +96,12 @@ def dashbaord_df():
     df.reset_index(inplace=True, drop=True)
     df = clean_WQ_data().merge(df, on='Date', how='left').merge(
         clean_met_data(), on='Date', how='left')
-
     return df
 
-
+# Main function to execute the program
 def main():
     df = dashbaord_df()
     print(df.to_striNG())
-
 
 if __name__ == "__main__":
     main()
